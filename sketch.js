49804a418ad81943ad129a8cc55fe223f3c78942
Hamza Ghandi <36565095+Hamza23579@users.noreply.github.com>
2020-06-19 23:22:11 +0100

    some weird looking annimation

--- sketch.js
+++ sketch.js
@@ -1,55 +1,54 @@
-/*
- * @name Array of Objects
- * @description Create a Jitter class, instantiate an array of objects
- * and move them around the screen.
- */
-let canvas;
-let button;
-var bugs = []; // array of Jitter objects
+
+let lines = [];
+let d = 15;
+let l;
 
 function setup() {
-  canvas = createCanvas(windowWidth, windowHeight);
-  canvas.position(0, 0);
-  canvas.style('z-index', '-1');
-  // button = createButton('Connect');
-  // button.style('background-color', color(20, 15, 255));
-  // button.style('background-color', '#4CAF50');
-  // button.style('padding', '15px 32px');
-  // button.style('font-size', '15px');
-  // button.style('border-radius', '15px');
-  // Create objects
-  for (var i=0; i<50; i++) {
-    bugs.push(new Lines());
+  createCanvas(400, 400);
+  for (let i = 0; i < width; i+=d){
+    for (let j = 0; j < height; j+=d){
+      lines.push(new Line(i, j, d))
+    }
   }
 }
 
 function draw() {
-  background(50, 89, 100);
-  for (var i=0; i<bugs.length; i++) {
-    bugs[i].move();
-    bugs[i].display();
+  background(25, 200, 25);
+  for (let i = 0; i < lines.length; i++){
+    lines[i].drawLine();
+    if (i !== lines.length - 1)
+      lines[i].move(lines[i + 1].y2);
+    else
+      lines[i].move(lines[i].y2);
   }
+  //console.log(lines[0].up);
 }
 
-// Jitter class
-function Lines() {
-  this.x1 = random(width);
-  this.x2 = random(width);
-  this.y1 = random(height);
-  this.y2 = random(height);
 
-  this.speed1 = random(-Math.PI, Math.PI);
-  this.speed2 = random(-Math.PI, Math.PI);
-  this.speed3 = random(-Math.PI, Math.PI);
-  this.speed4 = random(-Math.PI, Math.PI);
-  this.move = function() {
-    this.x1 += Math.sin(this.speed1);
-    this.x2 += Math.sin(this.speed2);
-    this.y1 += Math.sin(this.speed3);
-    this.y2 += Math.sin(this.speed4);
-  };
-
-  this.display = function() {
-    line(this.x1, this.x2, this.y1, this.y2);
-  };
+class Line {
+  
+  constructor(x, y, d){
+    this.x = x;
+    this.y1 = y;
+    this.y2 = y;
+    this.d = d;
+    this.up = 1;
+    this.down = -1;
+  }
+  
+  drawLine() {
+    line(this.x, this.y1, this.x + this.d, this.y2);
+  }
+  
+  move(y){
+    if (this.y2 > this.y1 + 10 || this.y2 < this.y1 - 10) {
+      this.up *= (-1);
+    }
+    if (this.y1 > y + 10 || this.y1 < y - 10) {
+      this.down *= (-1);
+    }
+    this.y2 = lerp(this.y2, this.y1 + 11 * this.up, 0.05);
+    this.y1 = lerp(y, this.y2 + 11 * this.down, 0.05);
+  }
+  
 }
